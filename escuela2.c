b7d9dcaa97a6a1e4d0b2e26f9a3778cbac3e580f
KevinQuinatoa <kevin.quinatoa.valladares@udla.edu.ec>
2025-11-17 16:49:39 -0500

    Update comment for PromedioC variable

--- escuela2.c
+++ escuela2.c
@@ -3,7 +3,7 @@
 int main() {
     char nombre[5][20];
     char asignatura[3][30];
-    float PromedioC[5][3] = {0}; // calificaciones por estudiante y asignatura
+    float PromedioC[5][3] = {0}; // calificaciones por estudiante A y asignatura
     float PromedioF[5] = {0};    // promedio final por estudiante 
     float maxEstudiante[5], minEstudiante[5];
     int aprobados[3] = {0}, reprobados[3] = {0};
