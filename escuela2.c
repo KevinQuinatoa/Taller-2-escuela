0be10bc5a4de725ba6940d9d5c4395dbfa276998
KevinQuinatoa <kevin.quinatoa.valladares@udla.edu.ec>
2025-11-17 16:59:06 -0500

    cambio

--- escuela2.c
+++ escuela2.c
@@ -3,7 +3,7 @@
 int main() {
     char nombre[5][20];
     char asignatura[3][30];
-    float PromedioC[5][3] = {0}; // calificaciones por estudiante y asignatura y cambio
+    float PromedioC[5][3] = {0}; // calificaciones por estudiante y asignatura y cambio todo lo demas
     float PromedioF[5] = {0};    // promedio final por estudiante
 
     float maxEstudiante[5], minEstudiante[5];
